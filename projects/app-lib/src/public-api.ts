5280b391239b62cb14dc30ccc0dd8180236bc614
Kien Alang <kien.alang@zuhlke.com>
2024-01-18 11:12:34 +0700

    Shareable Lib with hot module replacement

--- projects/app-lib/src/public-api.ts
+++ projects/app-lib/src/public-api.ts
@@ -5,4 +5,4 @@
 export * from './lib/app-lib.service';
 export * from './lib/app-lib.component';
 
-export * from './lib/not-found/not-found.component';
+export * from './lib/mui/button-group/button-group.component';
--- projects/customer/src/app/stock-picks/child-a/child-a.component.ts
+++ projects/customer/src/app/stock-picks/child-a/child-a.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatIconModule} from "@angular/material/icon";
+import {ButtonGroupComponent} from "app-lib";
 
 @Component({
   selector: 'app-child-a',
   standalone: true,
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink,
+    MatButtonModule, MatDividerModule, MatIconModule,
+    ButtonGroupComponent,
+  ],
   templateUrl: './child-a.component.html',
   styleUrl: './child-a.component.scss'
 })
